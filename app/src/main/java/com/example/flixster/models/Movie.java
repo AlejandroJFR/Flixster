e020f3db0875c057fcb781c59a9a3e303126224e
Alejandro J Figueroa Ramírez <alejandro.figueroa5@upr.edu>
2022-02-10 20:00:29 -0400

    Completed Part 2 required user stories

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,22 +3,35 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    String adultRating;
+    String releaseDate;
+    int movieId;
+    double rating;
+
+    //Empty constructor needed by the Parceler Library
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        adultRating = jsonObject.getString("adult");
+        releaseDate = jsonObject.getString("release_date");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -37,6 +50,18 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
 
+    public double getRating() {
+        return rating;
+    }
+
+    public String getAdultRating() {
+        return adultRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -44,6 +69,11 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
 
 
 }
